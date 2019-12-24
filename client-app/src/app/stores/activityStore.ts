99af9e70d3f47e78f7ea3f1f2c02a3fd60f6952f
Nha Nguyen <ntuannha@gmail.com>
2019-12-24 16:41:52 -0600

    Completed styling for pages

--- client-app/src/app/stores/activityStore.ts
+++ client-app/src/app/stores/activityStore.ts
@@ -16,9 +16,18 @@ class ActivityStore {
     // return this.activities.sort(
     //   (a, b) => Date.parse(a.date) - Date.parse(b.date)
     // );
-    return Array.from(this.activityRegistry.values()).sort(
+    return this.groupActivitiesByDate(Array.from(this.activityRegistry.values()));
+  }
+
+  groupActivitiesByDate(activities: IActivity[]){
+    const sortedActivities = activities.sort(
       (a, b) => Date.parse(a.date) - Date.parse(b.date)
     );
+    return Object.entries(sortedActivities.reduce((activities, activity) => {
+      const date = activity.date.split('T')[0];
+      activities[date] = activities[date] ? [...activities[date], activity] :  [activity];
+      return activities;
+    }, {} as {[key: string]: IActivity[]}));
   }
 
   @action loadActivities = async () => {
@@ -31,7 +40,8 @@ class ActivityStore {
           this.activityRegistry.set(activity.id, activity);
         });
         this.loadingInitial = false;
-        })
+        });
+        console.log(this.groupActivitiesByDate(activities));
     } catch (error) {
       runInAction('load activities error', () => {
         this.loadingInitial = false;
